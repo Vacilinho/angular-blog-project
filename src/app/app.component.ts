dd91f247aac524c180294fe1c2ff60d3ed43c73b
Vacilinho <rodrigoooki@gmail.com>
2024-09-10 14:04:59 -0300

    build: primeiros componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BigCardComponent } from "../component/big-card/big-card.component";
+import { MenuBarComponent } from "../component/menu-bar/menu-bar.component";
+import { SmallCardComponent } from "../component/small-card/small-card.component";
+import { MenuTitleComponent } from "../component/menu-title/menu-title.component";
+import { HomeComponent } from '../pages/home/home.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BigCardComponent, MenuBarComponent, SmallCardComponent, MenuTitleComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      'sans': 'Poppins, Arial, sans-serif',
+    },
   },
   plugins: [],
 }
