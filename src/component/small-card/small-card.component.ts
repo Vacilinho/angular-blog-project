7b100f88fbddad6a6c0111c092e37566bb71a890
Vacilinho <rodrigoooki@gmail.com>
2024-09-10 22:48:51 -0300

    build: small and big cards

--- src/component/small-card/small-card.component.ts
+++ src/component/small-card/small-card.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
   imports: [],
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
-
 export class SmallCardComponent {
+  @Input() cardPhoto: string = ' ';
+  @Input() cardTitle: string = ' ';
+  @Input() cardDescription: string = ' ';
 
 }
